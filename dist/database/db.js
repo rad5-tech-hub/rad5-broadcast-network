b5a9cd1df0f58a87aa65a8c2d41f8f7bd84bf15a
prince bassey <basseyprince75@gmail.com>
2025-06-04 13:17:56 +0100

    ignored dist file

--- dist/database/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-require("dotenv").config();
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, { host: process.env.DB_HOST, dialect: "mysql", logging: false });
-exports.default = sequelize;
