86d8bd6faba6003ec3bde284c256a7b25fd4428a
prince bassey <basseyprince75@gmail.com>
2025-06-10 11:52:25 +0100

    modified agent controller to return referral link and also modified verify email controller

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,16 +1,11 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-console.log("Cloudinary Keys:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "✓" : "✗ (missing)",
 });
 
 export default cloudinary;
